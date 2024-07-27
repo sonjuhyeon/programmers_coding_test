2bfc932c9b1bd3e85bd891490c1c8e164ef5ef27
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2024-07-27 23:46:31 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 6475.55 ms, Memory: 514 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/README.md
+++ 프로그래머스/2/62048. 멀쩡한 사각형/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀쩡한 사각형 - 62048 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/62048#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/62048) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 514 MB, 시간: 6475.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 27일 21:43:59
+2024년 07월 27일 23:46:28
 
 ### 문제 설명
 
--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,3 +1,5 @@
+import math
+
 def greatest_common_divisor(n, m):
     if m == 0:
         return n
@@ -5,10 +7,22 @@ def greatest_common_divisor(n, m):
         return greatest_common_divisor(m, n % m)
     
 def solution(w,h):
+    answer = 0
     area = w * h
     gcd = greatest_common_divisor(w, h)
-    scale = [w/gcd, h/gcd]
-    cut_block = (scale[0] * scale[1]) - ((scale[0] - 1) * (scale[1]-1))
+    if (w > h):
+        w, h = h, w
+    scale = [w//gcd, h//gcd]
+    ratio_lst = []
+    cut_block = 0
+    for i in range(1, scale[0] + 1):
+        ratio = scale[1] * i / scale[0]
+        ratio_lst.append(ratio)
     
-    answer = area - (cut_block * gcd)
+    for i in range(len(ratio_lst)):
+        if i == 0:
+            cut_block += (math.ceil(ratio_lst[i]))
+        else:
+            cut_block += (math.ceil(ratio_lst[i]) - math.floor(ratio_lst[i - 1]))
+    answer = area - (gcd * cut_block) 
     return answer
