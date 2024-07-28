00caacdff9782fa16119e6999a9cbb3d7fd8fde4
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2024-07-28 11:22:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,17 @@
+def greatest_common_divisor(n, m): #최소공배수
+    if m == 0:
+        return n
+    else:
+        return greatest_common_divisor(m, n % m)
+    
+def least_common_multiple(n, m):
+    gcd = greatest_common_divisor(n, m)
+    lcm = gcd * (n / gcd) * (m / gcd)
+    return (lcm)
+    
+def solution(arr):
+    for i in range(len(arr) - 1): # 2, 6, 8, 14
+        lcm = least_common_multiple(arr[0], arr[i + 1])
+        arr[0] = lcm
+    answer = arr[0]
+    return answer
