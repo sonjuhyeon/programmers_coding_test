7a6ba2ceefd1ff5eb28adf0fdba670e06c282209
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2024-08-17 20:36:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,7 +1,8 @@
 def solution(numbers):
-    tmp = []
+    answer = []
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
-            tmp.append(numbers[i] + numbers[j])
-    answer = sorted(set(tmp))
+            answer.append(numbers[i] + numbers[j])
+    answer = list(set(answer))
+    answer.sort()
     return answer
