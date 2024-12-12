bb284dd4c2c0f091083750fe9c309291fccc1f5f
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2024-12-12 12:36:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 188.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,41 +1,31 @@
-def check_open_bracket(char):
-    if char == '[' or char == '{' or char == '(':
-        return 1
-    else:
-        return 0
+def is_opening_bracket(char):
+    return char in '({['  # 여는 괄호인지 확인
 
-def check_bracket(sub):
-    ret = 0
+def is_balanced_brackets(substring):
     stack = []
-    for i in range(len(sub)):
-        if not stack:
-            if check_open_bracket(sub[i]):
-                stack.append(sub[i])
-                continue
-            else:
-                return 0
-        if (check_open_bracket(sub[i])):
-            stack.append(sub[i])
-            continue
+    for char in substring:
+        if is_opening_bracket(char):
+            stack.append(char)  # 여는 괄호는 스택에 추가
+        elif stack and (
+            (stack[-1] == '[' and char == ']') or
+            (stack[-1] == '{' and char == '}') or
+            (stack[-1] == '(' and char == ')')
+        ):
+            stack.pop()  # 닫는 괄호가 맞으면 스택에서 제거
         else:
-            if (
-                stack[-1] == '[' and sub[i] == ']' or
-                stack[-1] == '{' and sub[i] == '}' or
-                stack[-1] == '(' and sub[i] == ')'
-            ):
-                stack.pop()
-            else:
-                return 0
-    return 1
+            return False  # 잘못된 괄호가 있으면 균형이 맞지 않음
+    return not stack  # 스택이 비어 있으면 괄호가 균형을 이룸
 
 def solution(s):
-    len_s = len(s)
-    if (len_s % 2 == 1):
+    n = len(s)
+    if n % 2 == 1:  # 문자열 길이가 홀수이면 균형을 맞출 수 없음
         return 0
     
-    double_s = s * 2
-    answer = 0
-    for i in range(len_s):
-        sub = double_s[i:i + len_s]
-        answer += check_bracket(sub)
-    return answer
+    doubled_s = s * 2  # 문자열을 두 배로 늘려 회전된 문자열을 시뮬레이션
+    
+#     answer = 0
+#     for i in range(n):
+#         substring = doubled_s[i:i + n]
+#         answer += is_balanced_brackets(substring)
+#     return answer
+    return sum(is_balanced_brackets(doubled_s[i:i + n]) for i in range(n))
