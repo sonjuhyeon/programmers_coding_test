df6e7a6c904bd6bf35bbea4507acd0bc8ee33fc1
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2024-12-20 18:18:49 +0900

    [level 1] Title: 햄버거 만들기, Time: 242.99 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,9 +3,9 @@ def solution(ingredient):
     st = [] 
     for i in range(len(ingredient)):
         st.append(ingredient[i])
-        if(len(st)>=4 and ingredient[i] == 1): # 1 빵, 2 야채, 3 고기 
+        if(len(st) >= 4 and ingredient[i] == 1): #1:빵, 2:야채, 3:고기 
             n = len(st)
-            if st[-2] == 3 and st[-3] == 2 and st[-4]==1: #ingredient[n-1] == 1 and 
+            if st[-2] == 3 and st[-3] == 2 and st[-4] == 1:
                 for i in range(4):
                     st.pop()
                 answer+=1
