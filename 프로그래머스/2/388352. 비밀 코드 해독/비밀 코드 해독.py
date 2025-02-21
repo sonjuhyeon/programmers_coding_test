f5baa8afe335172fb176abc2792fe951d5f058e7
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-02-21 15:44:27 +0900

    [level 2] Title: 비밀 코드 해독, Time: 209.39 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,35 @@
+from itertools import combinations
+
+def solution(n, q, ans):
+    answer = 0
+    n_lst = [i for i in range(1, n+1)]
+    m = len(ans)
+    
+    # to_remove_index = []
+    # # 못 맞춘 케이스의 index 찾기
+    # for i in range(len(ans)):
+    #     if ans[i] == 0:
+    #         to_remove_index.append(i)
+    to_remove_index = list(filter(lambda i: ans[i] == 0, range(m)))
+    
+    # 하나도 못 맞춘 인덱스의 값 제거
+    for i in to_remove_index:
+        for j in q[i]:
+            try: lst.remove(j)
+            except: pass
+    
+    # 가능한 모든 조합의 리스트
+    # comb_lst = list(combinations(n_lst, 5))
+    
+    for c in combinations(n_lst, 5):
+        for i in range(m):
+            cnt = 0
+            for j in q[i]:
+                if j in c:
+                    cnt += 1
+            if cnt != ans[i]:
+                break
+        else: # break가 실행되지 않았을 경우 모든 조건 충족
+            answer += 1
+            
+    return answer
