56977a1fddd952be12f98f93b9a977165708dc8b
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-02-24 00:40:23 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,10 +1,6 @@
 def solution(n, w, num):
     answer = 0
     while (num <= n):
-        print(num)
-        if (num - 1 % w) == 0:
-            num += 2 * (w - ((num - 1) % w)) - 1
-        else:
-            num += 2 * (w - ((num - 1) % w)) - 1
+        num += 2 * (w - ((num - 1) % w)) - 1
         answer += 1
     return answer
