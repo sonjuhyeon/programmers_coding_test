fb83784adccf28868eaea318e478306de480c75c
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-02-26 16:47:00 +0900

    [level 2] Title: 구명보트, Time: 10.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,19 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    l = 0
-    r = len(people) - 1
-    while l <= r:
-        if r == l:
+    
+    min_idx = 0
+    max_idx = len(people) - 1
+    people_count = len(people)
+    
+    while people_count > 0:
+        if people_count == 1:
             answer += 1
             break
-        else:
-            if (people[l] + people[r] <= limit):
-                l += 1
-            r -= 1
-            answer += 1
+        if people[min_idx] + people[max_idx] <= limit:
+            min_idx += 1
+            people_count -= 1
+        max_idx -= 1
+        people_count -= 1
+        answer += 1
     return answer
