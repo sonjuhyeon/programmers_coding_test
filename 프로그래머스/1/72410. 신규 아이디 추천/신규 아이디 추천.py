973defbec0ae0f7f03f89aa615f8b76a21dc21c7
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-02-26 15:45:32 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,34 @@
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = list(new_id)
+    
+    for i in range(len(new_id) - 1, -1, -1):
+        if not (new_id[i].islower() or
+                new_id[i].isdigit() or
+                new_id[i] == '-' or 
+                new_id[i] == '_' or 
+                new_id[i] == '.'):
+            new_id.pop(i)
+    
+    for i in range(len(new_id) - 1, -1, -1):
+        if new_id[i] == '.' and new_id[i - 1] == '.' and i != 0:
+            new_id.pop(i)
+            
+    # print(new_id)
+    
+    if new_id[0] == '.':
+        new_id.pop(0)
+    if not new_id:
+        new_id.append('a')
+    if len(new_id) > 15:
+        new_id = new_id[0:15]
+    if new_id[-1] == '.':
+        new_id.pop()
+    if len(new_id) < 3:
+        for i in range(len(new_id) - 1, 2):
+            new_id.append(new_id[i])
+        
+    # print(new_id)
+    
+    answer = ''.join(new_id)
+    return answer
