1288560719c767e23a22ab76cdc2923198c9b9da
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-12 19:06:03 +0900

    [level 2] Title: 가장 큰 수, Time: 60.26 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,6 @@
 def solution(numbers):
-    max_length = max(map(len, map(str, numbers)))  # 가장 긴 숫자의 길이 계산
-    
     # 숫자를 문자열로 변환 후, 커스텀 정렬 (반복해서 붙였을 때 더 큰 값이 앞에 오도록 정렬)
-    numbers = sorted(map(str, numbers), key=lambda x: x * (max_length // len(x) + 1), reverse=True)
+    numbers = sorted(map(str, numbers), key=lambda x: x * 3, reverse=True)
     answer = "".join(numbers)
     
     if answer[0] == '0':
