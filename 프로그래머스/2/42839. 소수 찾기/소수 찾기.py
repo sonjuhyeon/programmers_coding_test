42b8c7c48f0e44be6f382b29a6fb207fc9452c1d
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-13 17:09:58 +0900

    [level 2] Title: 소수 찾기, Time: 1633.43 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,13 +1,16 @@
 from itertools import permutations
 from math import sqrt
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(sqrt(n)) + 1):  # 2부터 √n까지만 확인
-        if n % i == 0:
-            return False
-    return True
+def sieve(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0], is_prime[1] = False, False
+    
+    for i in range(2, int(sqrt(n)) + 1):
+        if is_prime[i] == True:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+                
+    return [i for i in range(n + 1) if is_prime[i]]
 
 def solution(numbers):
     answer = 0
@@ -16,8 +19,10 @@ def solution(numbers):
         for perm in permutations(list(numbers), i):
             perm_lst.add(int("".join(perm)))
     
+    primes = sieve(max(perm_lst))
+    
     for n in perm_lst:
-        if is_prime(n):
+        if n in primes:
             answer += 1
             
     return answer
