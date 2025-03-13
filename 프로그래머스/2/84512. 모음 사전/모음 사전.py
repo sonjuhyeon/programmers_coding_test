005a07d0ea9c020afe821ed8f22882bc2cadd291
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-14 02:00:12 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,18 @@
-from itertools import product
+# from itertools import product
+
+# def solution(word):
+#     vowel_dict = []
+#     words = ['A','E','I','O','U']
+#     for i in range(1,6):
+#         for j in product(words, repeat=i):
+#             vowel_dict.append(''.join(j))
+    
+#     vowel_dict.sort()
+#     answer = vowel_dict.index(word) + 1
+#     return answer
 
 def solution(word):
-    vowel_dict = []
-    words = ['A','E','I','O','U']
-    for i in range(1,6):
-        for j in product(words, repeat=i):
-            vowel_dict.append(''.join(j))
-    
-    vowel_dict.sort()
-    answer = vowel_dict.index(word) + 1
+    answer = 0
+    for i, n in enumerate(word):
+        answer += ((5 ** (5 - i) - 1) * "AEIOU".index(n) / 4) + 1
     return answer
