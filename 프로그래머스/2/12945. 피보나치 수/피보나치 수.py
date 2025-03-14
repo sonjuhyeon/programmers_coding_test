25cbeae4f0cd57ed863f94ce10284cffc8d94a36
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-15 02:51:16 +0900

    [level 2] Title: 피보나치 수, Time: 498.89 ms, Memory: 455 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,11 +1,14 @@
+def get_fibo(n, fibo):
+    for i in range(2, n + 1):
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
+        
+
 def solution(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
-
-    a, b = 0, 1  # f(0) = 0, f(1) = 1
-    for _ in range(2, n + 1):
-        a, b = b, a + b  # f(n-2) → f(n-1), f(n-1) → f(n)
-
-    return b % 1234567
+    fibo = [None] * (n + 1)
+    fibo[0], fibo[1] = 0, 1
+    get_fibo(n, fibo)
+    return fibo[n] % 1234567
