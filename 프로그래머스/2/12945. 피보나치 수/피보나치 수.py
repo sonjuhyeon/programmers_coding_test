0c0403b0989050155434b3c31301693ab3ce9ef0
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-15 03:08:08 +0900

    [level 2] Title: 피보나치 수, Time: 11.30 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,17 +1,17 @@
-def matrix_mult(A, B, mod=1234567):
-    return [[(A[0][0] * B[0][0] + A[0][1] * B[1][0]) % mod,
-             (A[0][0] * B[0][1] + A[0][1] * B[1][1]) % mod],
-            [(A[1][0] * B[0][0] + A[1][1] * B[1][0]) % mod,
-             (A[1][0] * B[0][1] + A[1][1] * B[1][1]) % mod]]
+def matrix_mult(A, B):
+    return [[A[0][0] * B[0][0] + A[0][1] * B[1][0],
+             A[0][0] * B[0][1] + A[0][1] * B[1][1]],
+            [A[1][0] * B[0][0] + A[1][1] * B[1][0],
+             A[1][0] * B[0][1] + A[1][1] * B[1][1]]]
 
-def matrix_power(mat, n, mod=1234567):
+def matrix_power(mat, n):
     res = [[1, 0], [0, 1]]  # 단위 행렬
     base = mat
     
     while n:
         if n % 2:  # 홀수이면 곱해줌
-            res = matrix_mult(res, base, mod)
-        base = matrix_mult(base, base, mod)  # 제곱
+            res = matrix_mult(res, base)
+        base = matrix_mult(base, base)  # 제곱
         n //= 2
     
     return res
@@ -25,4 +25,4 @@ def solution(n):
     F = [[1, 1], [1, 0]]  # 기본 행렬
     result = matrix_power(F, n - 1)
     
-    return result[0][0]  # F(n)
+    return result[0][0] % 1234567  # F(n)
