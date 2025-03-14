366a7fcbb91703d5e675b4b8e83d2f6a31791a7f
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-15 03:05:39 +0900

    [level 2] Title: 피보나치 수, Time: 0.04 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,14 +1,28 @@
-def get_fibo(n, fibo):
-    for i in range(2, n + 1):
-        fibo[i] = fibo[i - 1] + fibo[i - 2]
-        
+def matrix_mult(A, B, mod=1234567):
+    return [[(A[0][0] * B[0][0] + A[0][1] * B[1][0]) % mod,
+             (A[0][0] * B[0][1] + A[0][1] * B[1][1]) % mod],
+            [(A[1][0] * B[0][0] + A[1][1] * B[1][0]) % mod,
+             (A[1][0] * B[0][1] + A[1][1] * B[1][1]) % mod]]
+
+def matrix_power(mat, n, mod=1234567):
+    res = [[1, 0], [0, 1]]  # 단위 행렬
+    base = mat
+    
+    while n:
+        if n % 2:  # 홀수이면 곱해줌
+            res = matrix_mult(res, base, mod)
+        base = matrix_mult(base, base, mod)  # 제곱
+        n //= 2
+    
+    return res
 
 def solution(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
-    fibo = [None] * (n + 1)
-    fibo[0], fibo[1] = 0, 1
-    get_fibo(n, fibo)
-    return fibo[n] % 1234567
+
+    F = [[1, 1], [1, 0]]  # 기본 행렬
+    result = matrix_power(F, n - 1)
+    
+    return result[0][0]  # F(n)
