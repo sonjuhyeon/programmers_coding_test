ae2f8a76244f7e3a657e9e5c9981172d3e426f32
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-23 23:13:39 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 5476.80 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -0,0 +1,81 @@
+def get_path(route, points):
+    res = []
+        
+    for i in range(len(route) - 1):
+        start, end = points[route[i] - 1], points[route[i + 1] - 1]
+        row, col = start[0], start[1]
+        
+        # row 방향으로 먼저 이동
+        for _ in range(abs(start[0] - end[0])):
+            res.append([row, col])
+            if start[0] < end[0]:
+                row += 1
+            else:
+                row -= 1
+                
+        # column 방향이동
+        for _ in range(abs(start[1] - end[1])):
+            res.append([row, col])
+            if start[1] < end[1]:
+                col += 1
+            else:
+                col -= 1
+        
+    res.append([row, col])
+            
+    return res
+
+# 열의 길이가 다른 2차원 배열 전치
+def transpose_matrix(matrix):
+    max_len = max(len(row) for row in matrix) # 가장 긴 행의 길이
+
+    res = []
+    # 열 인덱스를 기준으로 순회
+    for i in range(max_len):
+        transposed_row = []
+        for row in matrix:
+            if i < len(row):
+                transposed_row.append(row[i])
+            else:
+                transposed_row.append(None) # 부족한 부분은 None으로 채움
+        res.append(transposed_row)
+    
+    return res
+
+def count_duplicates(arr):
+    counts = []
+
+    for val in arr:
+        if val is None:
+            continue  # None은 제외
+
+        found = False
+        for i in range(len(counts)):
+            if counts[i][0] == val:
+                counts[i][1] += 1
+                found = True
+                break
+        if not found:
+            counts.append([val, 1])
+
+    # 중복된 값 개수 세기 (2번 이상 등장)
+    duplicate_count = 0
+    for item, cnt in counts:
+        if cnt > 1:
+            duplicate_count += 1
+
+    return duplicate_count
+
+def solution(points, routes):
+    answer = 0
+    paths = []
+    
+    for route in routes:
+        paths.append(get_path(route, points))
+        
+    transposed_paths = transpose_matrix(paths)
+    
+    for path in transposed_paths:
+        answer += count_duplicates(path)
+        
+    return answer
