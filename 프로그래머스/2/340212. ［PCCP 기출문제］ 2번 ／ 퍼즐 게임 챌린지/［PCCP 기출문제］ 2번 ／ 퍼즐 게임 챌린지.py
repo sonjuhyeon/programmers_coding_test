8105c29edd160eb88ab3a31f2a22a63d995f444c
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-03-24 12:48:44 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1279.33 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,41 +1,27 @@
-def time_to_solve(level, diff, time_cur, time_prev):
-    # 숙련도가 난이도보다 높을 때
-    if level >= diff:
-        return time_cur
-    # 숙련도가 난이도보다 낮을 때
-    else:
-        return (diff - level) * (time_cur + time_prev) + time_cur
-
 def solution(diffs, times, limit):
-    # 최대 레벨, 최소 레벨을 설정합니다.
-    max_level, min_level = max(diffs), 1
+    max_level, min_level = max(diffs), 1  # 최대 레벨, 최소 레벨을 설정
 
-    # 최소 레벨이 최대 레벨과 같아질 때까지 반복합니다.
     while min_level < max_level:
-        # 중간 지점을 설정합니다.
+        # 중간 지점을 설정
         mid_level = (min_level + max_level) // 2
         mid_total = 0
 
-        # 모든 난이도에 대해 탐색합니다.
         for i in range(len(diffs)):
-            # 처음 위치에만 time_prev가 0
             if not i:
                 time_prev = 0
             else:
                 time_prev = times[i - 1]
 
-            # 현재 문제에 필요한 시간
-            time_cur = times[i]
-            mid_total += time_to_solve(mid_level, diffs[i], time_cur, time_prev)
+            time_cur = times[i] # 현재 문제에 필요한 시간
+            
+            if mid_level >= diffs[i]: # 숙련도가 난이도보다 높을 때
+                mid_total += time_cur
+            else: # 숙련도가 난이도보다 낮을 때
+                mid_total += (diffs[i] - mid_level) * (time_cur + time_prev) + time_cur
+    
+        if mid_total <= limit: # 중간 레벨이 제한 시간 내에 문제를 풀 수 있다면
+            max_level = mid_level # 최댓값을 내림
+        else: # 제한 시간 내에 문제를 풀 수 없다면
+            min_level = mid_level + 1 # 최솟값을 올림
 
-        # 중간 레벨이 제한 시간 내에 문제를 풀 수 있다면
-        # 최댓값을 내려줍니다.
-        if mid_total <= limit:
-            max_level = mid_level
-        # 제한 시간 내에 문제를 풀 수 없다면
-        # 최솟값을 올려줍니다.
-        else:
-            min_level = mid_level + 1
-
-    # 최소 레벨을 반환합니다.
     return min_level
