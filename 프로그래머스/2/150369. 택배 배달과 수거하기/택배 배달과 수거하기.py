4e064d6e2055fb2204a38093faf8fc3fe9e0cdf9
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-01 17:37:24 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2589.74 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -17,8 +17,8 @@ def get_distance(lst, cap):
 def solution(cap, n, deliveries, pickups):
     answer = 0
     
-    deliveries_list = [[i, deliveries[i]] for i in range(n) if deliveries[i] != 0]
-    pickups_list = [[i, pickups[i]] for i in range(n) if pickups[i] != 0]
+    deliveries_list = [[i, deliveries[i]] for i in range(n) if deliveries[i]]
+    pickups_list = [[i, pickups[i]] for i in range(n) if pickups[i]]
     
     while deliveries_list or pickups_list:
         delivery_distance = get_distance(deliveries_list, cap)
