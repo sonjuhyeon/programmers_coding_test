c66da6f989929ff25e4d7998e72c28ca5c9f5281
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-01 17:36:03 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1758.83 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,28 @@
+def get_distance(lst, cap):
+    farthest = 0
+    while lst:
+        dis, num = lst.pop()
+        if not farthest:
+            farthest = dis + 1
+        if num > cap:
+            num -= cap
+            lst.append([dis, num])
+            return farthest
+        elif num == cap:
+            return farthest
+        else:
+            cap -= num
+    return farthest
+
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    
+    deliveries_list = [[i, deliveries[i]] for i in range(n) if deliveries[i] != 0]
+    pickups_list = [[i, pickups[i]] for i in range(n) if pickups[i] != 0]
+    
+    while deliveries_list or pickups_list:
+        delivery_distance = get_distance(deliveries_list, cap)
+        pickup_distance = get_distance(pickups_list, cap)
+        answer += max(delivery_distance, pickup_distance) * 2
+
+    return answer
