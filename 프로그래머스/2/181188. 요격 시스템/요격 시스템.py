3b0f3c605647965cda7c447fc7020dbc9badffd0
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-02 10:15:43 +0900

    [level 2] Title: 요격 시스템, Time: 960.68 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,10 +1,10 @@
 def solution(targets):
     answer = 0
-    targets.sort(key=lambda x:x[1])
-    x = -1 # 요격 위치
+    targets.sort(reverse=True)
+    x = 100_000_001 # 요격 위치
     
     for s, e in targets:
-        if x < s:
+        if x > e:
             answer += 1
-            x = e - 0.5
+            x = s + 0.5
     return answer
