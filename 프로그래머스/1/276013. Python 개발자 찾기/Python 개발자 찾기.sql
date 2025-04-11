990b9255ee04eea076b1ac31861beb4642128cf9
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-12 00:29:09 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,7 +1,11 @@
 -- 코드를 작성해주세요
+# SELECT ID, EMAIL, FIRST_NAME, LAST_NAME FROM DEVELOPER_INFOS
+# WHERE
+#     SKILL_1 = 'Python' or
+#     SKILL_2 = 'Python' or
+#     SKILL_3 = 'Python'
+# ORDER BY ID
+
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME FROM DEVELOPER_INFOS
-WHERE
-    SKILL_1 = 'Python' or
-    SKILL_2 = 'Python' or
-    SKILL_3 = 'Python'
+WHERE 'Python' IN (SKILL_1, SKILL_2, SKILL_3)
 ORDER BY ID
