1127315c3a4b9d525a9d0a723606332e189a1509
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-18 21:16:26 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,32 +1,14 @@
 def solution(board, h, w):
     answer = 0
+    board_size = len(board)
+    dh = [0, 1, 0, -1] # row 변화
+    dw = [1, 0, -1, 0] # col 변화
     
-    directions = []#up, down, left, right
-    size = len(board)
-    value = board[h][w]
-    
-    directions.append([h-1, w])
-    directions.append([h+1, w])
-    directions.append([h, w-1])
-    directions.append([h, w+1])
-    print(directions)
-    #up
-    if (h-1 >= 0):
-        if board[directions[0][0]][directions[0][1]] == value:
-            answer += 1
-    
-    #left
-    if (w-1 >= 0):
-        if board[directions[2][0]][directions[2][1]] == value:
-            answer += 1
-    #down
-    if (h+1 < size):
-        if board[directions[1][0]][directions[1][1]] == value:
-            answer += 1
-    
-    #right
-    if (w+1 < size):
-        if board[directions[3][0]][directions[3][1]] == value:
-            answer += 1
-    
+    for row, col in zip(dh, dw):
+        current_row = h + row
+        current_col = w + col
+        
+        if (0 <= current_row < board_size) and (0 <= current_col < board_size):
+            if board[h][w] == board[current_row][current_col]:
+                answer += 1
     return answer
