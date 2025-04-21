4845f868be2b5e37cacc65c1362194e1e3df2ac2
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-21 21:42:02 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.26 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -4,20 +4,9 @@ def solution(survey, choices):
     personality = defaultdict(int)
     for sv, choice in zip(survey, choices):
         if choice <= 3:
-            if choice == 1:
-                personality[sv[0]] += 3
-            elif choice == 2:
-                personality[sv[0]] += 2
-            else:
-                personality[sv[0]] += 1
+            personality[sv[0]] += 4 - choice
         elif choice >= 5:
-            if choice == 5:
-                personality[sv[1]] += 1
-            elif choice == 6:
-                personality[sv[1]] += 2
-            else:
-                personality[sv[1]] += 3
-    # print(personality)
+            personality[sv[1]] += choice - 4
     
     answer = ''
     if personality['R'] >= personality['T']:
