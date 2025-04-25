82234363ecc4e5252dd312d549dae97d40088683
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-25 23:46:31 +0900

    [level 3] Title: 단속카메라, Time: 2.36 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -2,11 +2,10 @@ def solution(routes):
     answer = 1
     routes.sort()
     camera = routes[0][1]
-    print(routes)
+    
     for s, e in routes:
         if s > camera:
             answer += 1
             camera = e
-            print(camera)
         camera = min(camera, e)
     return answer
