4056cf226f8e076764266c183bbd6098e05c5b0c
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-04-28 15:28:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -3,14 +3,8 @@ from math import log
 def solution(n, a, b):
     answer = 0
     while a != b:
-        if a % 2 == 0:
-            a //= 2
-        else:
-            a = (a + 1) // 2
-        if b % 2 == 0:
-            b //= 2
-        else:
-            b = (b + 1) // 2
+        a = (a + 1) // 2
+        b = (b + 1) // 2
         answer += 1
 
     return answer
