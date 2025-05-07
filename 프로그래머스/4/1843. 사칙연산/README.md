8a03ac8d79c720685d041caa69bb82176307418a
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-05-07 18:48:07 +0900

    [level 4] Title: 사칙연산, Time: 0.28 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/README.md
+++ 프로그래머스/4/1843. 사칙연산/README.md
@@ -1,10 +1,10 @@
 # [level 4] 사칙연산 - 1843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1843) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1843#) 
 
 ### 성능 요약
 
-메모리: 9.42 MB, 시간: 0.31 ms
+메모리: 9.3 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 18:35:56
+2025년 05월 07일 18:48:04
 
 ### 문제 설명
 
--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -4,17 +4,16 @@ def solution(arr):
     
     min_tail, max_tail = 0, 0
     for i in range(len(arr) - 1, -1, -1):
-        tmp = arr[i].split('+')
+        tmp = list(map(int, arr[i].split('+')))
         if i != 0:
-            min_tmp = sum([-int(n) for n in tmp])
-            max_tmp = sum([int(n) if i >= 1 else -int(n) for i, n in enumerate(tmp)])
-            if i == len(arr) - 1:
-                min_tail = min_tmp
-                max_tail = max_tmp
-            else:
-                min_tail, max_tail = min(min_tmp + min_tail, min_tmp - max_tail), max(max_tmp + max_tail, min_tmp - min_tail)
+            min_tmp = -sum(tmp)
+            max_tmp = -2 * tmp[0] + sum(tmp)
+            min_tail, max_tail = (
+                min(min_tmp + min_tail, min_tmp - max_tail),
+                max(max_tmp + max_tail, min_tmp - min_tail)
+            )
         else:
             for n in tmp:
-                answer += int(n)
+                answer += n
     answer += max_tail
     return answer
