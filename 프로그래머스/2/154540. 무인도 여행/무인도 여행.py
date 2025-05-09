cac7d21331fe87ff6d3053f257cfdb6b5e73cd64
Son Ju-hyeon <97327228+sonjuhyeon@users.noreply.github.com>
2025-05-10 01:45:32 +0900

    [level 2] Title: 무인도 여행, Time: 14.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -6,31 +6,34 @@ def solution(maps):
     col_n = len(maps[0])
     
     visited = set()
+    dr, dc = [0, 0, 1, -1], [1, -1, 0, 0]
+    
     for i in range(row_n):
         for j in range(col_n):
+            # 방문했거나 'X'이면 건너뜀
             if (i, j) in visited or maps[i][j] == 'X':
                 continue
-            else:
-                dq = deque([(i, j)])
-                days = 0
-                dr, dc = [0, 0, 1, -1], [1, -1, 0, 0]
-                
-                while dq:
-                    r, c = dq.popleft()
-                    visited.add((r, c))
-                    for k in range(4):
-                        nr, nc = r + dr[k], c + dc[k]
-                        if (
-                            0 <= nr < row_n and 
-                            0 <= nc < col_n and 
-                            (nr, nc) not in visited and
-                            maps[nr][nc] != 'X'
-                        ):
-                            dq.append([nr, nc])
-                            visited.add((nr, nc))
-                        else:
-                            continue
-                    days += int(maps[r][c])
-                answer.append(days)
+            
+            # BFS 탐색 시작
+            dq = deque([(i, j)])
+            visited.add((i, j))  # 큐에 넣을 때 방문 처리
+            days = int(maps[i][j])  # 시작점의 값 누적
+            
+            while dq:
+                r, c = dq.popleft()
+                for k in range(4):
+                    nr, nc = r + dr[k], c + dc[k]
+                    if (
+                        0 <= nr < row_n and 
+                        0 <= nc < col_n and 
+                        (nr, nc) not in visited and
+                        maps[nr][nc] != 'X'
+                    ):
+                        dq.append((nr, nc))
+                        visited.add((nr, nc))
+                        days += int(maps[nr][nc])
+            
+            answer.append(days)
+    
     answer.sort()
     return answer if answer else [-1]
